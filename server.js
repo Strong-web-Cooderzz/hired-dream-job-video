d96e994e3a266d0210439e18eedfd5c8e43abc0e
unknown <mojahidislammishat@gmail.com>
2023-02-15 15:43:59 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+})
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello worldddd</h1>');
+});
+
+server.listen(5000, () => {
+  console.log('listening on *:5000');
+});
